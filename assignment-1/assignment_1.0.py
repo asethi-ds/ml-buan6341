1b49bda1410639156e5a438fd8684d55c592984d
asethi-ds <asethi@LAPTOP-SGHLL80B.localdomain>
2019-09-05 00:14:06 -0500

    variance inflation factor for dimensionality reduction updated

--- assignment-1/assignment_1.0.py
+++ assignment-1/assignment_1.0.py
@@ -1,3 +1,6 @@
+# Assignment 1
+# Author : asethi
+
 import pandas as pd 
 import numpy as np
 import datetime as dt
@@ -28,14 +31,17 @@ def feature_engineering(data):
     data["month"]       = data["date"].dt.month
     data["week_num"]    = data["date"].dt.week
     # Marking the flag if the launch day falls on the weekend
+    data['weekday']     = data['date'].dt.weekday
     data["is_weekend"]  = data["date"].dt.weekday.apply(lambda x: 1 if x > 5 else 0)
     data["hour_of_day"] = data["date"].dt.hour
     data['time_of_day'] = data["hour_of_day"].apply(assign_time_of_day)
     
-    feature_categorical_ohe = ['month','is_weekend','time_of_day']   
+    feature_categorical_ohe = ['month','is_weekend','time_of_day','weekday']   
     
     data = pd.get_dummies(data,columns = feature_categorical_ohe)
+    
     #data  = pd.get_dummies(data,columns=data[['month']])
+    #print(data.dtypes)
     return data
 
 
@@ -50,7 +56,17 @@ def assign_time_of_day(hour_val):
         time = 'night_time'
     return time
 
+
 def calculate_vif(X, thresh):
+    
+    feature_ohe_out = ['month_1', 'month_2', 'month_3', 'month_4', 'month_5', 'is_weekend_0','is_weekend_1',
+                'time_of_day_morning_time', 'time_of_day_night_time','time_of_day_sleep_time', 'time_of_day_work_hours', 'weekday_0',
+                    'weekday_1', 'weekday_2', 'weekday_3', 'weekday_4', 'weekday_5','weekday_6']
+
+    #for var in feature_ohe_out:
+     #   X[var]=X[var].astype(float)
+    #print((X.dtypes))
+    
     variables = list(range(X.shape[1]))
     dropped = True
     while dropped:
@@ -68,10 +84,11 @@ config_file_name='loc_config.ini'
 independent_original=['T1', 'RH_1', 'T2', 'RH_2', 'T3','RH_3', 'T4', 'RH_4', 'T5', 'RH_5', 'T6', 'RH_6', 'T7', 'RH_7', 'T8',
                         'RH_8', 'T9', 'RH_9', 'T_out', 'Press_mm_hg', 'RH_out', 'Windspeed','Visibility', 'Tdewpoint', 'rv1', 'rv2']
 
-independent_updated=['T1', 'RH_1', 'T2', 'RH_2', 'T3','RH_3', 'T4', 'RH_4', 'T5', 'RH_5', 'T6', 'RH_6', 'T7', 'RH_7', 'T8',
-                      'RH_8', 'T9', 'RH_9', 'T_out', 'Press_mm_hg', 'RH_out', 'Windspeed','Visibility', 'Tdewpoint', 'rv1', 'rv2', 'week_num',
-                      'hour_of_day','month_1', 'month_2', 'month_3', 'month_4', 'month_5', 'is_weekend_0','is_weekend_1', 'time_of_day_morning_time'                    , 'time_of_day_night_time','time_of_day_sleep_time', 'time_of_day_work_hours']
-
+independent_updated=['lights', 'T1', 'RH_1', 'T2', 'RH_2', 'T3','RH_3', 'T4', 'RH_4', 'T5', 'RH_5', 'T6',
+    'RH_6', 'T7', 'RH_7', 'T8','RH_8', 'T9', 'RH_9', 'T_out', 'Press_mm_hg', 'RH_out', 'Windspeed','Visibility', 'Tdewpoint',
+    'rv1', 'rv2', 'week_num', 'hour_of_day','month_1', 'month_2', 'month_3', 'month_4', 'month_5', 'is_weekend_0','is_weekend_1',
+    'time_of_day_morning_time', 'time_of_day_night_time','time_of_day_sleep_time', 'time_of_day_work_hours', 'weekday_0',
+    'weekday_1', 'weekday_2', 'weekday_3', 'weekday_4', 'weekday_5','weekday_6']
 
 energy_source_data=import_source_files(config_file_name)
 #print(energy_source_data.shape)
@@ -80,5 +97,4 @@ energy_source_data_features=feature_engineering(energy_source_data)
 
 #print(energy_source_data_features.dtypes)
 energy_source_data_workset=calculate_vif(energy_source_data_features[independent_updated],10.0)
-print(energy_source_data_workset.shape)
 
